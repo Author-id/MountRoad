dc570310d7f6729de4cd9e9e353f303cef4e7ed3
Andrey <semyanovandrew@yandex.ru>
2025-01-04 23:42:31 +0300

    add images

--- main.py
+++ main.py
@@ -1,1 +1,35 @@
 import pygame
+
+pygame.init()
+
+FPS = 60
+TILE_SIZE = 64
+WIDTH = 1250
+HEIGHT = 12 * TILE_SIZE
+
+screen = pygame.display.set_mode((WIDTH, HEIGHT))
+pygame.display.set_caption("Mountains")
+lvl = 1
+clock = pygame.time.Clock()
+
+hero_group = pygame.sprite.Group()
+tiles_group = pygame.sprite.Group()
+thorns_group = pygame.sprite.Group()
+flag_group = pygame.sprite.Group()
+all_sprites = pygame.sprite.Group()
+
+
+def start_screen():
+    pass
+
+
+def lvl_completed():
+    pass
+
+
+def game_over():
+    pass
+
+
+def finish_screen():
+    pass
